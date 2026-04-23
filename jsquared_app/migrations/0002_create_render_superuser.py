2d0f2bc4ca8cade48ce301d99f313779df183173
roblim8 <rob.lim@student.ateneo.edu>
2026-04-24 02:10:53 +0800

    create render superuser

---
+++ jsquared_app/migrations/0002_create_render_superuser.py
@@ -0,0 +1,33 @@
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+import os
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+
+    username = os.getenv("ADMIN_USERNAME")
+    password = os.getenv("ADMIN_PASSWORD")
+    email = os.getenv("ADMIN_EMAIL", "")
+
+    if not username or not password:
+        return
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create(
+            username=username,
+            email=email,
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            password=make_password(password),
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("jsquared_app", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
