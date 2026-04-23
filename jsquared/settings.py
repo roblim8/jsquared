46c8b587617bd9d067280419d3baf26ea930f96f
roblim8 <rob.lim@student.ateneo.edu>
2026-04-24 01:17:43 +0800

    fix static settings and template static paths

--- jsquared/settings.py
+++ jsquared/settings.py
@@ -114,8 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
